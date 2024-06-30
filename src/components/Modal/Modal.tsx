a67cc8966a260d8a2b0c49a9534e7c2811237a89
arthur andrade <arthurandrad.23@gmail.com>
2024-06-30 14:42:49 -0300

    Feat: Implemented win handle modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Modal.css';
+
+interface ModalProps {
+  isVisible: boolean;
+  onClose: () => void;
+  onRestart: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isVisible, onClose, onRestart }) => {
+  if (!isVisible) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="border text-center bg-light m-auto p-5 rounded">
+        <h1>Parabéns, você venceu!</h1>
+        <h4 className='my-4'>Gostaria de jogar mais uma vez?</h4>
+        <button className='btn btn-success m-2 px-5 py-3' onClick={onRestart}>Sim</button>
+        <button className='btn btn-danger m-2 px-5 py-3' onClick={onClose}>Não</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
