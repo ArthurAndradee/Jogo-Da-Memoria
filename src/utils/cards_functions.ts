c9b7d626d310257e9c1eccd7756df9a19b016dc6
arthur andrade <arthurandrad.23@gmail.com>
2024-06-30 13:37:32 -0300

    Fix: Reordered functions

--- src/utils/cards_functions.ts
+++ src/utils/cards_functions.ts
@@ -1,20 +1,21 @@
 import { CardProps } from "../components/Card";
 
-export const duplicateCards = <T>(array: T[]): T[] => {
-    return [...array, ...array];
-}
+export const resetCardsSort = (cards: CardProps[]): CardProps[] => {
+    return sortCards(regeranteCard(duplicateCards(cards)));
+} 
+
 export const sortCards = <T>(array: T[]): T[] => {
     return array.sort(() => Math.random() - 0.5);
-}
-
-export const randomKey = (): number => {
-    return Math.random() * 15;
 }
 
 export const regeranteCard = (cards:CardProps[]):CardProps[] =>{
     return cards.map((card) => ({...card,id:randomKey()}));
 }
 
-export const resetCardsSort = (cards: CardProps[]): CardProps[] => {
-    return sortCards(regeranteCard(duplicateCards(cards)));
-} 
+export const randomKey = (): number => {
+    return Math.random() * 115;
+}
+
+export const duplicateCards = <T>(array: T[]): T[] => {
+    return [...array, ...array];
+}
