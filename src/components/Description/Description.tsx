cbedaaabae3b2b5c870e096475bcd6209cf0edff
Arthur Andrade <arthurandrad.23@gmail.com>
2024-07-11 16:39:47 -0300

    fix

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Description.css'
 
 interface DescriptionProps {
@@ -8,14 +8,28 @@ interface DescriptionProps {
 }
 
 const Description: React.FC<DescriptionProps> = ({ isVisible, imageUrl, onClose }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (imageUrl) {
+      const img = new Image();
+      img.src = imageUrl;
+      img.onload = () => setLoading(false);
+    }
+  }, [imageUrl]);
 
   if (!isVisible) return null;
 
   return (
     <div className='description-cards-modal'>
       <div className='description-cards-content'>
+        {loading ? (
+          <div className='loading-spinner'>
+            Loading...
+          </div>
+        ) : (
           <img src={imageUrl} alt="description cards" />
-
+        )}
         <button className='btn btn-dark btn-lg w-25' onClick={onClose}>Fechar</button>
       </div>
     </div>
