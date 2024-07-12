99baed1cd3eb22bbeb38bbe603d8dd67bce5843d
arthur andrade <arthurandrad.23@gmail.com>
2024-07-12 00:23:04 -0300

    Fix: Testing new method for image displaying

--- src/components/Restart/Restart.tsx
+++ src/components/Restart/Restart.tsx
@@ -6,11 +6,11 @@ interface RestartProps {
 }
 
 function Restart({ isVisible, onClick }: RestartProps) {
-    if (!isVisible) return null;
+    if (isVisible) return null;
 
     return (
-        <button className='btn btn-light' onClick={onClick}>
-            Restart
+        <button className='btn btn-secondary' onClick={onClick}>
+            Jogar de novo
         </button>
     );
 }
