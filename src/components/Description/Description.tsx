f4d50a17caf7970184a3fef3aabb9c1876836459
arthur andrade <arthurandrad.23@gmail.com>
2024-07-12 01:23:54 -0300

    Test: Changing HTML tag text

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -25,7 +25,7 @@ const Description: React.FC<DescriptionProps> = ({ isVisible, imageUrl, onClose 
       <div className='description-cards-content'>
         {loading ? (
           <div className='loading-spinner'>
-            Loading...
+            Still Loading...
           </div>
         ) : (
           <img src={imageUrl} alt="description cards" />
