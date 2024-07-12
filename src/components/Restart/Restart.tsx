8603f502f8e842cf241d515cec0328e17a50f32c
arthur andrade <arthurandrad.23@gmail.com>
2024-07-12 01:55:06 -0300

    Fix: Fixed Restart component logic

--- src/components/Restart/Restart.tsx
+++ src/components/Restart/Restart.tsx
@@ -6,7 +6,7 @@ interface RestartProps {
 }
 
 function Restart({ isVisible, onClick }: RestartProps) {
-    if (!isVisible) return null;
+    if (isVisible) return null;
 
     return (
         <button className='btn btn-light' onClick={onClick}>
